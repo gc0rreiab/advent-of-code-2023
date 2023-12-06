bbd97a93d7a7a8789050fbe13318d9c6d34abb77
gcb <gabriel.brito.95@gmail.com>
2023-12-06 22:36:25 +0000

    Remove debug comments. Clean code

--- day1_Trebuchet/challenge.py
+++ day1_Trebuchet/challenge.py
@@ -12,7 +12,7 @@ numeric_digits_and_words_dict = {
         "9": "nine"
 }
 
-#function to part one
+#function to solve part one
 def extract_numeric_digits_from_string(input_str):
         extracted_digits = ""
         for char in input_str:
@@ -20,28 +20,33 @@ def extract_numeric_digits_from_string(input_str):
                         extracted_digits += char
         return extracted_digits
 
-#function to part two
+#function to solve part two
 def extract_numeric_digits_and_numeric_words_from_string(input_str):
         extracted_digits = ""
         i = 0
         i_ant = i
         while i < len(input_str):
-                if input_str[i].isdigit():
-                        extracted_digits += input_str[i]
-                else:
-                        for digit, word in numeric_digits_and_words_dict.items():
-                                if input_str.startswith(word, i):
-                                        extracted_digits += digit
-                                        i = i+len(word) - 1
-                                        print("here")
-                                        break
+                digit_found = False
+                for digit, word in numeric_digits_and_words_dict.items():
+                        if input_str.startswith(digit, i):
+                                digit_found = True
+                                i += 1
+                        elif input_str.startswith(word, i):
+                                digit_found = True
+                                i += (len(word) - 1)
+
+                        if digit_found == True:
+                                extracted_digits += digit
+                                break
                 if i == i_ant:
                         i += 1
-                i_ant = i      
+                i_ant = i
+                                
         return extracted_digits
 
 
-#Solution starts here
+
+#Puzzle solution starts here
 
 file = open('input.txt', 'r')
 lines = file.readlines()
@@ -58,19 +63,18 @@ for line in lines:
                 extracted_digits = extract_numeric_digits_from_string(line)
         else:
                 extracted_digits = extract_numeric_digits_and_numeric_words_from_string(line)
-
+            
         calibration_values = extracted_digits[0] + extracted_digits[(len(extracted_digits))- 1]
+        sum += int(calibration_values)
+
         #for debug purposes
         print(line.strip())
         print(extracted_digits)
         print(calibration_values)
-
-        sum += int(calibration_values)
         print()
-
+        
 #Result
-print()
-print("Sum Result: ")
+print("Result: ")
 print(sum)
 
 
