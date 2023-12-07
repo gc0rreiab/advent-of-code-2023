386ca5f802129be3e277c2fc68a5cc09d2a5a03c
gcb <gabriel.brito.95@gmail.com>
2023-12-07 19:18:16 +0000

    Day 2, Part 2 complete

--- day2_Cube-Conundrum/challenge.py
+++ day2_Cube-Conundrum/challenge.py
@@ -4,6 +4,8 @@ import re
 #Debug in terminal with Colors
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk), end=" ")
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk), end=" ")
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk),  end=" ")
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk),  end=" ")
 
 elf_bag = { "red"   : 12,
             "green" : 13,
@@ -22,8 +24,13 @@ class Game:
 file = open('input.txt', 'r')
 lines = file.readlines()
 
+#To solve only the first part, change it to False. Otherwise keep True to solve part two too.
+part_two = True
+
 #Each line is a Game
 sum = 0
+if part_two : sum2 = 0
+
 for line in lines:
     id, sep, sub_games = line.strip().partition(': ')
     garbage, sep, id = id.partition(' ')
@@ -32,60 +39,61 @@ for line in lines:
     G = Game(id, 0, 0, 0)
     sub_games = sub_games.split("; ")
     
-    prRed("Game: ")
-    print(line)
-    prRed("ID: ")
+    prPurple("Game: ")
+    print(line.strip())
+    prPurple("ID: ")
     print(id)
-    prRed("Sub Games: ")
+    prPurple("Sub Games: ")
     print(sub_games)
-    print()
     i = 0
     for sub_game in sub_games:
         i += 1
         sub_game = sub_game.split(', ')
-        prRed("Sub Game" + str(i) + ": ")
-        print(sub_game, end=" ")
-        print()
-        j = 0
         #Dictionary to store the number of cubes for each color in each subgame  
-        res_dct = {}
+        cube_dct = {}
         for cubes in sub_game:
-            j += 1
             quantity, sep, color = cubes.partition(' ')
-            res_dct[f"{color}"] = int(quantity)
-            prRed("Val " + str(j) + ": ")
-            print(color + " : " + quantity)
+            cube_dct[f"{color}"] = int(quantity)
 
-        prRed("Dict: ")
-        print(res_dct, end=" ")
+        prPurple("Sub Game " + str(i) + " Dict: ")
+        print(cube_dct, end=" ")
         print()
         
-        game_is_possible = True
-        for key in res_dct.keys():
-            if(res_dct[key] > elf_bag[key]):
-                game_is_possible = False
-                break
-            else:
-                game_is_possible = True
+        for key in cube_dct.keys():
+            if(cube_dct[key] > G.cubes[key]):
+                G.cubes[key] = cube_dct[key]
 
-        if game_is_possible == False:
+
+    game_is_possible = False
+    for key in elf_bag.keys():
+        if (G.cubes[key] > elf_bag[key]):
+            game_is_possible = False
             break
-        
+        else:
+            game_is_possible = True
+       
     if game_is_possible:
         prGreen("YES !")
         sum = sum + G.id
     else:
         prRed("NO !")
 
+    if part_two:
+        aux = 1
+        for key in elf_bag.keys():
+            aux =  aux * G.cubes[key]
+        sum2 = sum2 + aux
+            
     print()
     print()      
     
-
-print()
-prRed("Sum: ")
+prYellow("Part I solution:\t")
 print(sum)
+if part_two is True:
+    prYellow("Part II solution:\t")
+    print(sum2)
+            
         
-
 
 
 
